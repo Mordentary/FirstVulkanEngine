4befa52333d772c4bd5202083fbcce5d45cf4096
Mordentary <seva.batalin@gmail.com>
2024-05-22 22:49:02 +0300

    Chore: Setup Premake script for postbuild asset copying and configuration

--- premake5.lua
+++ premake5.lua
@@ -1,26 +1,43 @@
 workspace "VulkanEngine"
-    configurations { "Debug", "Release" }
+    configurations { "Debug", "Release", "Dist" }
     architecture "x64"
-	startproject "VulkanEngine"
-	configurations
-    {
-		"Debug",
-		"Release",
-		"Dist"
-	}
-	flags
-	{
-		"MultiProcessorCompile"
-	}
+    startproject "VulkanEngine"
 
-outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
-IncludeDir = {}
-IncludeDir ["Vulkan"] = "C:/VulkanSDK/1.3.243.0/Include"
-IncludeDir["GLFW"] = "VulkanEngine/vendor/GLFW/include"
-IncludeDir["glm"] = "VulkanEngine/vendor/glm"
-IncludeDir["stb_image"] = "VulkanEngine/vendor/stb_image"
-IncludeDir["TinyLoader"] = "VulkanEngine/vendor/TinyObjLoader"
+    flags { "MultiProcessorCompile" }
 
+    outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+    IncludeDir = {}
+    IncludeDir["Vulkan"] = "C:/VulkanSDK/1.3.243.0/Include"
+    IncludeDir["GLFW"] = "VulkanEngine/vendor/GLFW/include"
+    IncludeDir["glm"] = "VulkanEngine/vendor/glm"
+    IncludeDir["stb_image"] = "VulkanEngine/vendor/stb_image"
+    IncludeDir["TinyLoader"] = "VulkanEngine/vendor/TinyObjLoader"
+
+
+    files{
+        "VulkanEngine/shaders/src/**.vert",
+        "VulkanEngine/shaders/src/**.frag",
+        "VulkanEngine/assets/**.*"
+    }
+    
+  filter "configurations:Debug"
+        postbuildcommands {
+            '{COPY} "%{wks.location}/VulkanEngine/assets" "%{cfg.buildtarget.directory}/assets"',
+            '{COPY} "%{wks.location}/VulkanEngine/shaders/bin" "%{cfg.buildtarget.directory}/shaders/bin"'
+        }
+        
+    filter "configurations:Release"
+        postbuildcommands {
+            '{COPY} "%{wks.location}/VulkanEngine/assets" "%{cfg.buildtarget.directory}/assets"',
+            '{COPY} "%{wks.location}/VulkanEngine/shaders/bin" "%{cfg.buildtarget.directory}/shaders/bin"'
+        }
+        
+    filter "configurations:Dist"
+        postbuildcommands {
+            '{COPY} "%{wks.location}/VulkanEngine/assets" "%{cfg.buildtarget.directory}/assets"',
+            '{COPY} "%{wks.location}/VulkanEngine/shaders/bin" "%{cfg.buildtarget.directory}/shaders/bin"'
+        }
 
 rule "ShaderCompilation"
     location "VulkanEngine/shaders"
@@ -30,6 +47,7 @@ rule "ShaderCompilation"
     buildcommands { 'glslc.exe "%(FullPath)" -o "shaders/bin/%(Filename)%(Extension).spv"' }
     buildoutputs { "VulkanEngine/shaders/bin/%(Filename)%(Extension).spv" }
 
+
 project "VulkanEngine"
     kind "ConsoleApp"
     language "C++"
@@ -37,103 +55,70 @@ project "VulkanEngine"
     rules { "ShaderCompilation" }
     cppdialect "C++20"
 
-    targetdir("bin/" .. outputdir ..  "/%{prj.name}")
-	objdir("bin-int/" .. outputdir ..  "/%{prj.name}")
+    targetdir("bin/" .. outputdir .. "/%{prj.name}")
+    objdir("bin-int/" .. outputdir .. "/%{prj.name}")
 
     pchheader "pch.h"
-	pchsource "VulkanEngine/src/pch.cpp"
+    pchsource "VulkanEngine/src/pch.cpp"
 
-    files 
-	{
-		"%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.cpp",
-		"%{prj.name}/vendor/stb_image/**.cpp",
-		"%{prj.name}/vendor/stb_image/**.h",
-		"%{prj.name}/vendor/TinyObjLoader/**.cpp",
-		"%{prj.name}/vendor/TinyObjLoader/**.h",
+    files {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp",
+        "%{prj.name}/vendor/stb_image/**.cpp",
+        "%{prj.name}/vendor/stb_image/**.h",
+        "%{prj.name}/vendor/TinyObjLoader/**.cpp",
+        "%{prj.name}/vendor/TinyObjLoader/**.h",
         "%{prj.name}/vendor/glm/glm/**.hpp",
-		"%{prj.name}/vendor/glm/glm/**.inl",
-        "%{prj.name}/shaders/src/**.frag",
-        "%{prj.name}/shaders/src/**.vert",
-        "%{prj.name}/shaders/CompileShaders.bat"
+        "%{prj.name}/vendor/glm/glm/**.inl"
     }
 
-    includedirs
-	{
-		"%{prj.name}/src",
-		"%{IncludeDir.GLFW}",
-		"%{IncludeDir.stb_image}",
-		"%{IncludeDir.glm}",
+
+    includedirs {
+        "%{prj.name}/src",
+        "%{IncludeDir.GLFW}",
+        "%{IncludeDir.stb_image}",
+        "%{IncludeDir.glm}",
         "%{IncludeDir.Vulkan}",
         "%{IncludeDir.TinyLoader}"
     }
-   
 
-    libdirs
-    {
+    libdirs {
         "%{prj.name}/vendor/GLFW/lib-vc2022",
-        "C:/VulkanSDK/1.3.243.0/Lib",
+        "C:/VulkanSDK/1.3.243.0/Lib"
     }
-    
-    links
-    {
+
+    links {
         "glfw3.lib",
-        "vulkan-1.lib",
+        "vulkan-1.lib"
     }
 
-
-    
-    -- local compileShadersScript = [[
-    --     @echo off
-
-    --     setlocal enabledelayedexpansion
-
-    --     set "shader_dir=shaders/src"
-    --     set "output_dir=shaders/bin"
-    --     set "compiler=glslc.exe"
-
-    --     for %%f in ("%shader_dir%\\*.vert" "%shader_dir%\\*.frag") do (
-    --         set "filename=%%~nf"
-    --         set "extension=%%~xf"
-    --         %compiler% "%%f" -o "%output_dir%\\!filename!!extension!.spv"
-    --     )
-
-    --     endlocal
-    --     ]]
-
-  
-
-    -- prebuildcommands
-    -- {
-    --     compileShadersScript
-    -- }
-
-    -- Set up the project for shader sorcery.
 
     filter "system:windows"
         systemversion "latest"
-
-        defines 
-        { 
-            "GLM_FORCE_DEPTH_ZERO_TO_ONE", 
-            "GLM_FORCE_RADIANS" 
+        defines {
+            "GLM_FORCE_DEPTH_ZERO_TO_ONE",
+            "GLM_FORCE_RADIANS"
         }
-    
-    
+
     filter "configurations:Debug"
-        defines "_DEBUG"
+        defines "DEBUG"
         runtime "Debug"
         symbols "On"
-        flags {"NoIncrementalLink" }
-        targetname "VulkanTest_Debug"
-    
+        flags { "NoIncrementalLink", "LinkTimeOptimization" }
+        editandcontinue "Off"
+        linkoptions { "/fsanitize=address" }
+        targetname "VulkanEngine_Debug"
+
     filter "configurations:Release"
-        defines "_RELEASE"
+        defines "RELEASE"
+        runtime "Release"
+        symbols "On"
+        optimize "Debug"
+        targetname "VulkanEngine_Release"
+
+    filter "configurations:Dist"
+        defines "DIST"
         runtime "Release"
         symbols "Off"
         optimize "Full"
-        targetname "VulkanTest_Release"
-
-
-
-       
+        targetname "VulkanEngine_Dist"
